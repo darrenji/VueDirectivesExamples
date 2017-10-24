5a341152fdff98369b3ff6a7ae9990c4056eb6ef
darren <764190362@qq.com>
2017-10-24 22:59:12 +0800

    通过Directive把事件类型和事件传值给html元素

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directive Exercise</h1>
-                <button v-customOn="clicked">Click Me</button>
+                <button v-customOn:click="clicked">Click Me</button>
             </div>
         </div>
     </div>
@@ -68,9 +68,13 @@
             customOn: {
                 
                 bind(el, binding){
-                    el.onclick = function(){
-                        binding.value();
-                    }
+//                    el.onclick = function(){
+//                        binding.value();
+//                    }
+                    
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
                 }
             }
         },
