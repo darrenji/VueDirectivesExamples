66a9ce581eacc7232d5002309aa1398899e5922d
darren <764190362@qq.com>
2017-10-24 14:36:04 +0800

    v-highlight:background.delayed中delayed叫作modifiers,在directive中，被放到了binding.modifiers上

--- src/main.js
+++ src/main.js
@@ -12,11 +12,21 @@ Vue.directive('highlight',{
         //el.style.backgroundColor = 'green';
         //el.style.backgroundColor = binding.value;
         
-        if(binding.arg == 'background'){
-            el.style.backgroundColor = binding.value;
-        } else {
-            el.style.color = binding.value;
+        var delay = 0;
+        
+        if(binding.modifiers['delayed']){
+            delay = 3000;
         }
+        
+        setTimeout(() => {
+            if(binding.arg == 'background'){
+                el.style.backgroundColor = binding.value;
+            } else {
+                el.style.color = binding.value;
+            }
+        },delay);
+        
+        
     }
 });
 
