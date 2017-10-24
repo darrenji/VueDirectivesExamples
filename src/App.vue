0a4795fbb2610e6892b8aa4d336b2a508be35501
darren <764190362@qq.com>
2017-10-24 23:11:25 +0800

    练习结束

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,13 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directive Exercise</h1>
                 <button v-customOn:click="clicked">Click Me</button>
+                
+                <hr>
+                <div 
+                    style="width:100px;height:100px;background-color:lightgreen"
+                    v-customOn:mouseenter="mouseEnter"
+                    v-customOn:mouseleave="mouseLeave"                       
+                    ></div>
             </div>
         </div>
     </div>
@@ -81,6 +88,12 @@
         methods: {
             clicked(){
                 alert('I was clicked');
+            },
+            mouseEnter(){
+                console.log('mouse entered');
+            },
+            mouseLeave(){
+                console.log('mouse leave');
             }
         }
     }
